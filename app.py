e65bd379768606b7681cd705b821f6d0c21b35b5
Tomasz Kołtuniak <tomaszkoltuniak22@gmail.com>
2023-06-29 15:04:52 +0200

    google configuration update

--- app.py
+++ app.py
@@ -162,9 +162,8 @@ def logout():
 
 
 def get_google_provider_cfg():
-    return requests.get(
-        "https://accounts.google.com/.well-known/openid-configuration"
-    ).json()
+    config = requests(os.environ.get("GOOGLE_DISCOVERY_URL")) or "https://accounts.google.com/.well-known/openid-configuration"
+    return config.json()
 
 
 @app.route("/add", methods=["GET", "POST"])
